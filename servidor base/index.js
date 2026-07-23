4431e6bbcde2018abb1cafcba2600e57e0ce5486
David Grande Ibarra <lotuseveryday55555@gmail.com>
2026-07-23 15:54:03 -0700

    Ejericios servidor

---
+++ servidor base/index.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+const PORT = 3000;
+
+let peliculas = [
+    {
+        id: 1,
+        titulo: "Batman",
+        director: "Christopher Nolan",
+        año: 2008
+    }, 
+    {
+        id: 2,
+        titulo: "Interestelar",
+        director: "Christopher Nolan",
+        año: 2024
+    }
+]
+
+let idActual = peliculas.length + 1;
+
+//Obtener todas las peliculas
+app.get("/peliculas", (req, res) => {
+    res.json(peliculas);
+})
+
+//Obtener una pelicula por ID
+app.get("/peliculas/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const pelicula = peliculas.find(
+        pelicula => pelicula.id === id
+    );
+    if(!pelicula){
+        return res.status(404).json({
+            mensaje: "Pelicula no encontrada"
+        });
+    }
+    res.json(pelicula);
+});
+
+//Registrar una pelicula nueva o crear
+app.post("/peliculas", (req, res) => {
+    const {titulo, director, año} = req.body;
+    if(!titulo || !director || !año){
+        return res.status(400).json({
+            mensaje : "Faltan datos de la pelicula"
+        });
+    };
+
+    const nuevaPelicula = {
+        id: idActual++,
+        titulo: titulo,
+        director: director,
+        año: Number(año)
+    };
+    peliculas.push(nuevaPelicula);
+    res.status(201).json({
+        mensaje : "Pelicula registrada correctamente",
+        pelicula : nuevaPelicula
+    });
+});
+
+//Actualizar pelicula
+app.put("/peliculas/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const {titulo, director, año} = req.body;
+    if(!titulo || !director || !año){
+        return res.status(400).json({
+            mensaje : "Faltan datos de la pelicula"
+        });
+    }
+    const indice = peliculas.findIndex(
+        pelicula => pelicula.id === id
+    );
+
+    if(indice === -1){
+        return res.status(404).json({
+            mensaje : "Pelicula no encontrada"
+        });
+    }
+
+    peliculas[indice] = {
+        id: id,
+        titulo: titulo,
+        director: director, 
+        año: Number(año)
+    };
+
+    res.json({
+        mensaje : "Pelicula actualizada correctamente",
+        pelicula: peliculas[indice]
+    });
+
+});
+
+
+//Eliminar una pelicula
+app.delete("/peliculas/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const indice = peliculas.findIndex(
+        pelicula => pelicula.id === id
+    );
+    if(indice === -1){
+        return res.status(404).json({
+            mensaje : "Pelicula no encontrada"
+        });
+    }
+    const peliculaEliminada = peliculas[indice];
+
+    peliculas.splice(indice, 1);
+
+    res.json({
+        mensaje : "Pelicula eliminada correctamente",
+        pelicula : peliculaEliminada
+    });
+});
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log("Servidor iniciado en http://localhost:"+PORT);
+})
+
